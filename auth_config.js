40e0a2f68cfe3ba6d16527dfd05f7be4c4e503a3
Denis Osipov <admin@kenig.org>
2021-03-05 16:13:55 +0700

    add auth

--- auth_config.js
+++ auth_config.js
@@ -1,8 +1,10 @@
-console.log(process);
+console.log(self.process);
+console.log(window.process);
+console.log(this.process);
 
-const config = {
-    domain: process.env.domain,
-    clientId: process.env.clientId
-};
-
-export default config;
+// const config = {
+//     domain: process.env.domain,
+//     clientId: process.env.clientId
+// };
+//
+// export default config;
