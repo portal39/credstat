686d7b1c6ea7400b20ec757dd48abc39850583df
Denis Osipov <admin@kenig.org>
2021-03-05 16:15:40 +0700

    ++

--- auth_config.js
+++ auth_config.js
@@ -2,9 +2,9 @@ console.log(self.process);
 console.log(window.process);
 console.log(this.process);
 
-// const config = {
-//     domain: process.env.domain,
-//     clientId: process.env.clientId
-// };
-//
-// export default config;
+const config = {
+    domain: "",
+    clientId: ""
+};
+
+export default config;
