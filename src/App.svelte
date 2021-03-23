c4385dec4537cb3061c5d7db26acbbb13f4bc574
Denis Osipov <admin@kenig.org>
2021-03-23 19:46:48 +0700

    ++++++

--- src/App.svelte
+++ src/App.svelte
@@ -167,7 +167,7 @@
 </script>
 
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-	<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">CredStat</a>
+	<span class="navbar-brand col-md-3 col-lg-2 me-0 px-3">CredStat</span>
 	<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
@@ -176,7 +176,7 @@
 	{#if $isAuthenticated}
 	<ul class="navbar-nav px-3">
 		<li class="nav-item text-nowrap">
-			<a class="nav-link" href="#" on:click={LogOut}>Sign out</a>
+			<a class="nav-link" on:click={LogOut}>Sign out</a>
 		</li>
 	</ul>
 	{:else}
