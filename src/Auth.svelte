91de72f66e17760d3ed14b90f7dd2e4db70d9a7b
Denis Osipov <admin@kenig.org>
2021-04-09 18:47:19 +0700

    xxxdx

--- src/Auth.svelte
+++ src/Auth.svelte
@@ -63,7 +63,7 @@
     }
 </script>
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <span class="navbar-brand col-md-3 col-lg-2 me-0 px-3">CredStat v.1.0.1</span>
+    <span class="navbar-brand col-md-3 col-lg-2 me-0 px-3">CredStat v.1.0.2</span>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
