7971485a1839c33ac0dfc8210dddec37a72ae886
Denis Osipov <admin@kenig.org>
2021-04-16 13:27:22 +0700

    1-0-3

--- src/Srez.svelte
+++ src/Srez.svelte
@@ -6,8 +6,10 @@
     onDestroy(async () => {
         if(interval)clearInterval(interval);
     })
+    let myChart = false;
     onMount(async () => {
         isAuthenticated.subscribe(load);
+
     });
     async function load(isAuth){
 
@@ -19,6 +21,15 @@
             clearInterval(interval);
         }
     }
+    const colors = [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(255, 206, 86, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(153, 102, 255, 0.5)'
+    ];
+    let labels = [];
+    let dataset = [];
 
     function request(callback,data){
         fetch('https://api.swcred.ru/stat/', {
@@ -35,11 +46,60 @@
     async function reload(){
         request((response)=>{
             srez_ = response.srez;
+
+            if(response.dataset) {
+
+                    labels = response.dataset.labels;
+                    Object.values(response.dataset.set).forEach(function (item, i, arr) {
+
+                        dataset.push({
+                            label: response.dataset.label[i],
+                            backgroundColor: colors[i],
+                            borderColor: colors[i],
+                            data: item,
+                        })
+
+                    });
+
+                    if (!myChart) {
+                        myChart = new Chart(
+                            document.getElementById('myChart'),
+                            {
+                                type: 'bar',
+                                data: {
+                                    labels: labels,
+                                    datasets: dataset
+                                },
+                                options: {
+                                    responsive: true,
+                                    plugins: {
+                                        legend: {
+                                            position: 'top',
+                                        },
+                                        title: {
+                                            display: true,
+                                            text: 'SREZZ'
+                                        }
+                                    }
+                                },
+                            }
+                        );
+                        dataset = [];
+                    }else {
+                        myChart.data.labels = labels;
+                        myChart.data.datasets = dataset;
+                        myChart.update();
+                    }
+
+            }
         },JSON.stringify({
             action: 'getSrez',
             // filter: filter
         }))
     }
+
+
+
 </script>
 
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -47,6 +107,9 @@
 </div>
 
 <section>
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
     <table class="table table-striped table-hover table-sm table-bordered">
         <thead>
         <tr>
@@ -68,4 +131,6 @@
         {/each}
         </tbody>
     </table>
+
+
 </section>
